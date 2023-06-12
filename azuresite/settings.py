af546dd1312a17001cc67c539fcb916c3fc2e028
Njita Arnaud <arnaudnjita@gmail.com>
2023-06-12 18:02:55 +0100

    updated db conns

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,25 +79,25 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES={
-   'default':{
-    #   'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME':os.getenv('DB_NAME'),
-      'USER':os.getenv('DB_USER'),
-      'PASSWORD':os.getenv('DB_PASSWORD'),
-      'HOST':os.getenv('DB_HOST'),
-      'PORT':'5432',
-      'OPTIONS': {'sslmode': 'require'}
-   }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES={
+#    'default':{
+#     #   'ENGINE':'django.db.backends.postgresql_psycopg2',
+#       'ENGINE': 'django.db.backends.postgresql',
+#       'NAME':os.getenv('DB_NAME'),
+#       'USER':os.getenv('DB_USER'),
+#       'PASSWORD':os.getenv('DB_PASSWORD'),
+#       'HOST':os.getenv('DB_HOST'),
+#       'PORT':'5432',
+#       'OPTIONS': {'sslmode': 'require'}
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
