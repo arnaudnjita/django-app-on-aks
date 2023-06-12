07006b601b3ab4a99a60e4b0883e4d4a648c6782
Njita Arnaud <arnaudnjita@gmail.com>
2023-06-12 15:24:23 +0100

    django-app-on-aks

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
-from pathlib import Path
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,13 +78,25 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES={
+   'default':{
+    #   'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME':os.getenv('DB_NAME'),
+      'USER':os.getenv('DB_USER'),
+      'PASSWORD':os.getenv('DB_PASSWORD'),
+      'HOST':os.getenv('DB_HOST'),
+      'PORT':'5432',
+      'OPTIONS': {'sslmode': 'require'}
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
